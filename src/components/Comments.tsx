fcb91eca6f8d44636ba754589e6d23f09ad3e812
sushaz9 <susannahelisabeth1@gmail.com>
2023-11-29 14:51:55 +0000

    edit form

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,4 +1,5 @@
 import { WEB_SITE } from "config";
+import { CommentForm } from "./CommentForm";
 
 export default async function Comments({ postSlug }: { postSlug: string }) {
   // `/blog
@@ -16,18 +17,8 @@ export default async function Comments({ postSlug }: { postSlug: string }) {
 
   return (
     <div>
+      <CommentForm postSlug={postSlug} />
       <h2>| Comments |</h2>
-      <h3>Leave a comment: </h3>
-
-      <form action={`/api/comments/${postSlug}`} method="POST">
-        <label htmlFor="username">Name:</label>
-        <input type="text" name="username" />
-
-        <label htmlFor="comment">Your comment:</label>
-        <textarea name="comment" cols={30} rows={10} />
-
-        <button type="submit">send comment</button>
-      </form>
       {/* @ts-ignore */}
       {comments.map((comment) => {
         return (
