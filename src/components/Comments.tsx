305445ea6f69a0a86f1fd2ca6f73698c113f4141
sushaz9 <susannahelisabeth1@gmail.com>
2023-11-29 09:50:04 +0000

    website url

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,14 +1,15 @@
+import { WEB_SITE } from "config";
+
 export default async function Comments({ postSlug }: { postSlug: string }) {
   // `/blog
 
-  const WEBSITE_URL = "http://localhost:3000";
+  // const WEBSITE_URL = "http://localhost:3000";
 
   let comments = [];
   try {
-    const commentsResult = await fetch(
-      `${WEBSITE_URL}/api/comments/${postSlug}`,
-      { next: { revalidate: 5 } }
-    );
+    const commentsResult = await fetch(`${WEB_SITE}/api/comments/${postSlug}`, {
+      next: { revalidate: 5 },
+    });
     const response = await commentsResult.json();
     comments = response.comments.rows;
   } catch (err) {}
